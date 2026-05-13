c52f255231fd28d06fe7ac61b0bb9d20de6582d7
Javier Ros <jros@jros-Azken-Work.tail363b98.ts.net>
2026-05-13 15:52:09 +0200

    "Double Pendulum example with corrent notation"

--- Class_Notes_Web/examples/temperature_sensor_in_a_room/inject_matlab_preamble.m
+++ Class_Notes_Web/examples/temperature_sensor_in_a_room/inject_matlab_preamble.m
@@ -2,17 +2,22 @@ figure('visible','off');
 set(gcf, 'ToolBar', 'none');
 
 warning('off','MATLAB:print:AxesToolbarInExport');
-% 1. Set all text (Labels, Titles, Legends) to use LaTeX by default
 
+% 1. Set all text (Labels, Titles, Legends) to use LaTeX by default
 set(groot,'defaultAxesTickLabelInterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 set(groot,'defaultTextInterpreter','latex');
-% 2. Set the Axes (Tick labels) to use LaTeX by default
-set(groot, 'DefaultAxesTickLabelInterpreter', 'latex');
 
-% 3. Set Font Units and Size to be proportional (Normalized)
-set(groot, 'DefaultAxesFontUnits', 'normalized');
-set(groot, 'DefaultAxesFontSize', 0.05);
+% 2. Lock units to 'points' to prevent disproportionate scaling
+set(groot, 'DefaultAxesFontUnits', 'points');
+set(groot, 'DefaultTextFontUnits', 'points');
+set(groot, 'DefaultLegendFontUnits', 'points');
+
+% 3. Set a uniform Font Size for all elements
+uniform_fontsize = 14; % Adjust this single number to scale the whole plot's text
+set(groot, 'DefaultAxesFontSize', uniform_fontsize);
+set(groot, 'DefaultTextFontSize', uniform_fontsize);
+set(groot, 'DefaultLegendFontSize', uniform_fontsize);
 
 % 4. Set default Line Width for all plots
-set(groot, 'DefaultLineLineWidth', 2);
+set(groot, 'DefaultLineLineWidth', 1);
